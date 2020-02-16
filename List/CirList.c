a6b57bc70e9221bb5df154f014870194501887b1
lemon8888 <lemon12321@163.com>
2020-02-16 18:29:38 +0800

    list

--- List/CirList.c
+++ List/CirList.c
@@ -1,5 +1,15 @@
 //循环链表
 #include <stdio.h>
+#include <stdlib.h>
+typedef struct node{
+    int data;
+    struct node* next;
+}LinkList;
+
+LinkList *head=NULL;
+LinkList *end=NULL;
+int num=0;//除头节点个数
+
 int main(){
     printf("的撒循环链表");
     return 0;
